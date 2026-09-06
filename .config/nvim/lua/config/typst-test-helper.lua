f3aa9e62610b9a7774dba98a5fc19948576bbc7f
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2026-09-06 02:55:56 +0200

    feat(nvim): update typst-test-helper keymaps

--- .config/nvim/lua/config/typst-test-helper.lua
+++ .config/nvim/lua/config/typst-test-helper.lua
@@ -7,13 +7,16 @@ local function on_attach(buf)
     wk.add({
         buffer = buf,
         { "<leader>it",  group = "Typst" },
-        { "<leader>itt", tth.map_run_this(),                   desc = "Run test" },
-        { "<leader>its", tth.map_run_this({ "--scale=4" }),    desc = "Run test scale=4" },
-        { "<leader>itu", tth.map_run_this({ "--update" }),     desc = "Update ref" },
-        { "<leader>itr", tth.map_open_render("identity"), desc = "Open render" },
-        { "<leader>ith", tth.read_html,                   desc = "Read html" },
-        { "<leader>itp", tth.read_pdftags,                desc = "Read pdftags" },
-        { "<leader>itv", tth.read_svg,                    desc = "Read svg" },
+        { "<leader>itt", tth.map_run_this(),                desc = "Run test" },
+        { "<leader>its", tth.map_run_this({ "--scale=4" }), desc = "Run test scale=4" },
+        { "<leader>itu", tth.map_run_this({ "--update" }),  desc = "Update ref" },
+        { "<leader>itr", tth.map_open_render("identity"),   desc = "Open render" },
+        { "<leader>ith", tth.open_html,                     desc = "Open html" },
+        { "<leader>itH", tth.read_html,                     desc = "Read html" },
+        { "<leader>itp", tth.open_pdf,                      desc = "Open pdf" },
+        { "<leader>itP", tth.read_pdftags,                  desc = "Read pdftags" },
+        { "<leader>itv", tth.open_svg,                      desc = "Open svg" },
+        { "<leader>itV", tth.read_svg,                      desc = "Read svg" },
     })
 end
 
